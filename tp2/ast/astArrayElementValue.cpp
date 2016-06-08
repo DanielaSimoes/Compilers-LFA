baa51f9d28dcd50b0f2d1de3d704876f9b519ed0
Rui Lopes <ruieduardo.fa.lopes@me.com>
2016-06-08 19:28:08 +0100

    Bugfix on astArrayElementValue - missing a semicolon

--- tp2/ast/astArrayElementValue.cpp
+++ tp2/ast/astArrayElementValue.cpp
@@ -15,7 +15,7 @@ void ASTArrayElementValue::generateLSM(FILE* fout)
     fprintf(stdout, "ASTArrayElementValue\n");
     fprintf(fout, "%15s; accessing integer element value on array %s\n", " ", label.c_str());
     if (ASTNode::text == 0) {
-        fprintf(fout, "%15s.text", " ")
+        fprintf(fout, "%15s.text", " ");
         if (getType() == INT) {
             fprintf(fout, "%15sla %s\n", " ", label.c_str());
             fprintf(fout, "%15siadd %d\n", " ", offset);
