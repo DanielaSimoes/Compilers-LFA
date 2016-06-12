f1817428c158b30bbb50f37e63d0a51af5cda67d
Rui Lopes <ruieduardo.fa.lopes@me.com>
2016-06-12 23:01:39 +0100

    Removed show instructions on printstr

--- tp2/ast/astPrintStr.cpp
+++ tp2/ast/astPrintStr.cpp
@@ -10,8 +10,6 @@ int ASTPrintStr::gcnt = 0;
 
 void ASTPrintStr::show(uint32_t indent) {
     fprintf(stdout, ";%*s ASTPrintStr\n", indent, "");
-    if (entryPoint == 2)
-        value -> show(++indent);
 }
 
 void ASTPrintStr::generateLSM(FILE* fout) {
